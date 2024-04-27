72fccbf1c7a61ce1ac0c9c16ad909f12ebcefad9
Cecília Martins De Oliveira <135181058+oliivercm@users.noreply.github.com>
2024-04-26 21:12:11 -0300

    implementei a funcionalidade de adicionar contatos
    apaguei as outras funções, pois, elas não iriam dar certo, falta corrigir algumas coisas, vou implementar a próxima em outra branch.

--- agenda.c
+++ agenda.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TAM_TELEFONE 15
+#define TAM_TELEFONE 16
 #define FILENAME "agenda.bin"
 
 void clearBuffer() {
@@ -28,71 +28,13 @@ ERROS adicionar(Agenda agenda[], int *pos) {
     clearBuffer();
 
     printf("Digite o telefone: ");
-    scanf("%14s", agenda[*pos].Telefone);
+    scanf("%15s", agenda[*pos].Telefone);
     clearBuffer();
 
     (*pos)++;
+    printf("contato salvo com sucesso!!\n");
     return OK;
 }
 
-ERROS listar(Agenda agenda[], int *pos) {
-    if (*pos == 0) {
-        return SEM_CONTATOS;
-    }
 
-    printf("Lista de contatos:\n");
-    for (int i = 0; i < *pos; i++) {
-        printf("Nome: %s %s, Email: %s, Telefone: %s\n", agenda[i].Nome, agenda[i].Sobrenome, agenda[i].Email, agenda[i].Telefone);
-    }
 
-    return OK;
-}
-
-ERROS deletar(Agenda agenda[], int *pos) {
-    if (*pos == 0) {
-        return SEM_CONTATOS;
-    }
-
-    char telefone[TAM_TELEFONE];
-    printf("Digite o número de telefone do contato que deseja deletar: ");
-    scanf("%14s", telefone);
-    clearBuffer();
-
-    int encontrado = 0;
-    for (int i = 0; i < *pos; i++) {
-        if (strcmp(agenda[i].Telefone, telefone) == 0) {
-            encontrado = 1;
-            for (int j = i; j < *pos - 1; j++) {
-                agenda[j] = agenda[j + 1];
-            }
-            (*pos)--;
-            break;
-        }
-    }
-
-    return encontrado ? OK : NAO_ENCONTRADO;
-}
-
-ERROS salvar(Agenda agenda[], int *pos) {
-    FILE *arquivo = fopen(FILENAME, "wb");
-    if (arquivo == NULL) {
-        return ERRO_ARQUIVO;
-    }
-
-    fwrite(agenda, sizeof(Agenda), *pos, arquivo);
-    fclose(arquivo);
-
-    return OK;
-}
-
-ERROS carregar(Agenda agenda[], int *pos) {
-    FILE *arquivo = fopen(FILENAME, "rb");
-    if (arquivo == NULL) {
-        return ERRO_ARQUIVO;
-    }
-
-    *pos = fread(agenda, sizeof(Agenda), TOTAL, arquivo);
-    fclose(arquivo);
-
-    return OK;
-}
