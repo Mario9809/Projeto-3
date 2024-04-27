466a6a9a190944c69679da4ebbd77f6af6f2fdb4
Mario Eugenio Silva <161092737+Mario9809@users.noreply.github.com>
2024-04-26 21:44:41 -0300

    verificação na main.c

--- main.c
+++ main.c
@@ -1,37 +1,75 @@
 #include "agenda.h"
 #include <stdio.h>
 
-#define TAM_NOME 300
-#define TAM_SOBRENOME 300
-#define TAM_EMAIL 300
+
 
 int main() {
+   
 
-  funcao fs[] = {criar, listar, deletar, salvar, carregar};
+    Agenda agenda[TOTAL];
+    int pos;
+    ERROS erro = carregar(agenda, &pos); 
+    if (erro != OK)
+        pos = 0;
 
-  Agenda agenda[TOTAL];
-  int pos;
-  ERROS erro = fs[4](agenda, &pos);
-  if (erro != OK)
-    pos = 0;
-
-
-
-  int opcao;
-  do {
-    printf("\nMenu principal\n");
-    printf("1 - Adicionar contato\n");
-    printf("2 - Listar contatos\n");
-    printf("3 - Deletar contato\n");
-    printf("4 - Salvar contato\n");
-    printf("5 - Carregar contato");
-    printf("0 - Sair\n");
-    printf("Escolha uma opcao: ");
+    int opcao;
+    do {
+        printf("\nMenu principal\n");
+        printf("1 - Adicionar contato\n");
+        printf("2 - Listar contatos\n");
+        printf("3 - Deletar contato\n");
+        printf("4 - Salvar contato\n");
+        printf("5 - Carregar contato\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
 
 
-    scanf("%d", &opcao);
-    opcao--;
+      switch (opcao) {
+
+          case 1:
+
+              adicionar(agenda, &pos);
+
+              break;
+
+          case 2:
+
+              listar(agenda, &pos);
+
+              break;
+
+          case 3:
+
+              deletar(agenda, &pos);
+
+              break;
+
+          case 4:
+
+              salvar(agenda, &pos);
+
+              break;
+
+          case 5:
+
+              carregar(agenda, &pos);
+
+              break;
+
+          case 0:
+
+              printf("Saindo...\n");
+
+              break;
+
+          default:
+
+              printf("Escolha inválida.\n");
+
+      }
 
     } while (opcao != 0);
 
+    return 0;
 }
